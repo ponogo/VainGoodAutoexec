4409194bfbf9e8e6a44699c7b336042fbcf6d40d
Pavleg <pajabonki@gmail.com>
2020-07-23 17:08:28 +0000

    update

--- main.py
+++ main.py
@@ -18,13 +18,30 @@ def triangle(length, color):
     x+=1
   bob.end_fill()
 
-input_shape = input("what shape do you want to draw?")
+def drawCircle(radius, color):
+  bob.fillcolor(color)
+  bob.begin_fill()
+  bob.circle(int(radius))
+  bob.end_fill()
+
+def star(length, color):
+  bob.fillcolor(color)
+  bob.begin_fill()
+  x=0
+  while x < 5:
+    bob.forward(int(length))
+    bob.right(144)
+    x+=1
+  bob.end_fill
+
+
+input_shape = input("what shape do you want to draw? ")
 input_length =  input("chooses how big: ")
 input_color = input("Choose color: ")
 
 if input_shape == "triangle":
   triangle(input_length, input_color)
 elif input_shape == "circle":
-  print("circle")
+  drawCircle(input_length, input_color)
 elif input_shape == "star":
-  print("star")  
+  star(input_length, input_color)  
